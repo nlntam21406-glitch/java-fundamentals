5e2598002936732c46f943a966fb2ef5865efdd6
Nhật Tâm <nlntam21406@gmail.com>
2026-05-25 22:17:49 +0700

    1

---
+++ src/week01/day01/Task1_DataTypeExplorer.java
@@ -0,0 +1,23 @@
+package week01.day01;
+
+public class Task1_DataTypeExplorer {
+    public static void main(String[] args) {
+        String fullName = "Nhat Tam";
+        int age = 20;
+        long studentID = 52401866666666666L;
+        double gpa = 8.3;
+        char point = 'A';
+        boolean sex = true;
+
+        System.out.println("=== Student Information === ");
+        System.out.println("Full name: " + fullName);
+        System.out.println("Age: " + age);
+        System.out.println("Student ID: " + studentID);
+        System.out.println("GPA: " + gpa);
+        System.out.println("Point: " + point);
+        System.out.println("Sex: " + sex);
+
+    }
+
+}
+
---
+++ src/week01/day01/Task2_IntegerFlow.java
@@ -0,0 +1,13 @@
+package week01.day01;
+
+public class Task2_IntegerFlow {
+    public static void main(String[] args) {
+        int max = Integer.MAX_VALUE;
+        System.out.println("Max value: " + max);
+        int min = Integer.MIN_VALUE;
+        System.out.println("Min value: " + min);
+
+        int overflow = max + 1;
+        System.out.println("Overflow value: " + overflow);
+    }
+}
